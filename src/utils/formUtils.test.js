212e567e3d3bb9154bb0ffd1c90b5f57fc2a7493
Anca Raduta <anca.raduta@3pillarglobal.com>
2020-03-10 14:31:15 +0200

    react sessions #16 - unit tests - added a describe to wrap the isFormFieldValid tests

--- src/utils/formUtils.test.js
+++ src/utils/formUtils.test.js
@@ -1,27 +1,29 @@
 import {isFormFieldValid} from "./formsUtils";
 
 describe("formUtils", () => {
-  it("tests that the method isFormFieldValid correctly validates an email field", () => {
-    const fieldId = "email";
-    const value = "anca@gmail.com";
-    expect(isFormFieldValid(fieldId, value)).toEqual(true);
-  });
+  describe("isFormFieldValid", () => {
+    fit("tests that the method isFormFieldValid correctly validates an email field", () => {
+      const fieldId = "email";
+      const value = "anca@gmail.com";
+      expect(isFormFieldValid(fieldId, value)).toEqual(true);
+    });
 
-  it("tests that the method isFormFieldValid does not validate an erroneous email address", () => {
-    const fieldId = "email";
-    const value = "anca r.";
-    expect(isFormFieldValid(fieldId, value)).toEqual(false);
-  });
+    fit("tests that the method isFormFieldValid does not validate an erroneous email address", () => {
+      const fieldId = "email";
+      const value = "anca r.";
+      expect(isFormFieldValid(fieldId, value)).toEqual(false);
+    });
 
-  it("tests that the method isFormFieldValid correctly validates a password field", () => {
-    const fieldId = "password";
-    const value = "pass123";
-    expect(isFormFieldValid(fieldId, value)).toEqual(true);
-  });
+    it("tests that the method isFormFieldValid correctly validates a password field", () => {
+      const fieldId = "password";
+      const value = "pass123";
+      expect(isFormFieldValid(fieldId, value)).toEqual(true);
+    });
 
-  it("tests that the method isFormFieldValid does not validate a password field if empty", () => {
-    const fieldId = "password";
-    const value = "";
-    expect(isFormFieldValid(fieldId, value)).toEqual(false);
+    it("tests that the method isFormFieldValid does not validate a password field if empty", () => {
+      const fieldId = "password";
+      const value = "";
+      expect(isFormFieldValid(fieldId, value)).toEqual(false);
+    });
   });
 });
